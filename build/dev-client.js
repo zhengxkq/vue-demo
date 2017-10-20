b4a8b511e18d8cc04b1f6bebcd104bb13742b57e
zhengxkq <zhengxkq@163.com>
2017-10-20 11:43:38 +0800

    add dev-client

---
+++ build/dev-client.js
@@ -0,0 +1,5 @@
+var config = require('../config')
+if(!process.env.NODE_ENV) process.env.NODE_ENV = JSON.parse(config.dev.NODE_ENV)
+var path = require('path')
+var express = require('express')
+var webpack = require('webpack')
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,5 +10,24 @@ Object.keys(baseWebpackConfig.entry).forEach(function(name){
 })
 
 module.exports = merge(baseWebpackConfig, {
-    
+    module:{
+        loaders:utils.styleLoaders({
+            sourceMap:config.dev.cssSourceMap
+        })
+    },
+    devtool:'#eval-source-map',
+    plugins:[
+        new webpack.DefinePlugin({
+            'process.env':config.dev.env
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            filename:'index.html',
+            template:'index.html',
+            favicon:'favicon.ico',
+            inject:true
+        })
+    ]
 })
