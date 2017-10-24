fc05f676794b8322dcdee10e8970a0d386765de1
zhengxkq <zhengxkq@163.com>
2017-10-24 23:52:23 +0800

    add home

---
+++ src/page/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <head-top signin-up="home">
+          <span slot="logo" class="head_logo" @click="reload">ele.me</span>
+      </head-top>
+  </div>
+</template>
+
+<script>
+import headTop from '../../components/header/head'
+
+export default {
+    
+    components:{
+        headTop
+    }
+}
+</script>
+
