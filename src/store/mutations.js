abbc15ce23706ce1914fc23d407f93685ea15eb3
zhengxkq <zhengxkq@163.com>
2017-11-01 23:10:25 +0800

    mutations

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,30 @@
 import {
+    RECORD_USERINFO,
     GET_USERINFO,
     SAVE_ADDRESS
 } from './mutation-types'
 
+import {setStore, getStore} from '../config/mUtils'
+
+import {localapi,proapi} from 'src/config/env'
+
+export default {
+    [RECORD_USERINFO](state, info){
+        state.userInfo = info;
+        state.login = true;
+        setStore('user_id', info.user_id);
+    },
+    [GET_USERINFO](state, info){
+        if(state.userInfo && (state.userInfo.username !== info.username)){
+            return;
+        }
+        if(!state.login){
+            return;
+        }
+        if(!info.message){
+            state.userInfo = {...info};
+        }else{
+            state.userInfo = null;
+        }
+    }
+}
